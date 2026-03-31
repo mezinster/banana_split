af71065173679687638f7b62f9c28b1e9829d356
Evgeny Mezin <evgeny.mezin@syngenta.com>
2026-03-31 18:15:05 +0400

    feat: add fork attribution and source code link to About screen

--- banana_split_flutter/lib/screens/about_screen.dart
+++ banana_split_flutter/lib/screens/about_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:banana_split_flutter/screens/privacy_policy_screen.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -30,6 +31,30 @@ class AboutScreen extends StatelessWidget {
           Text(l10n.aboutSecurityNotes, style: textTheme.titleMedium),
           const SizedBox(height: 8),
           Text(l10n.aboutSecurityNotesBody, style: textTheme.bodyMedium),
+          const SizedBox(height: 16),
+          Text(
+            l10n.aboutForkNotice('banana_split', 'Parity Technologies'),
+            style: textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            l10n.aboutForkCopyright,
+            style: textTheme.bodySmall,
+          ),
+          const SizedBox(height: 8),
+          InkWell(
+            onTap: () => launchUrl(
+              Uri.parse('https://github.com/paritytech/banana_split'),
+              mode: LaunchMode.externalApplication,
+            ),
+            child: Text(
+              'github.com/paritytech/banana_split',
+              style: textTheme.bodySmall?.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
           const Divider(height: 32),
           FutureBuilder<PackageInfo>(
             future: PackageInfo.fromPlatform(),
@@ -72,6 +97,15 @@ class AboutScreen extends StatelessWidget {
                 ),
               );
             },
+          ),
+          ListTile(
+            leading: const Icon(Icons.code),
+            title: Text(l10n.aboutSourceCode),
+            trailing: const Icon(Icons.open_in_new),
+            onTap: () => launchUrl(
+              Uri.parse('https://github.com/mezinster/banana_split'),
+              mode: LaunchMode.externalApplication,
+            ),
           ),
         ],
       ),
